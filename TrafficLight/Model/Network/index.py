eb46687b9aa7acca04ee6088d74864d275afb638
KlaussMarchi <marchiklauss@hotmail.com>
2026-05-05 15:49:22 -0300

    reset de novo

--- TrafficLight/Model/Network/index.py
+++ TrafficLight/Model/Network/index.py
@@ -1,39 +1,46 @@
 import torch
-import torch.optim as optim
 import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-from .jaccard.index import JaccardIndex # Sua métrica customizada
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+from torchvision.ops import nms
+import numpy as np
 
-class ModelNetwork:
-    def __init__(self, network, img_size, classes=1, channels=3, lr=1e-4, dropout=0.1, num_filters=16):
-        self.network = network
+from .jaccard.index import JaccardIndex
+
+
+class Network:
+    def __init__(self, name='', img_size=256, classes=1, channels=3, dropout=0.1, num_filters=32, lr=0.001):
+        self.name   = name
         self.img_size = img_size
         self.classes  = classes
-        self.multiclass = (self.classes > 1)
-        self.channels   = channels
-        self.dropout    = dropout
+        self.multiclass  = (self.classes > 1)
+        self.channels    = channels
+        self.dropout     = dropout
         self.num_filters = num_filters
-        self.lr = lr
+        self.lr  = lr
         
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.model  = self.get().to(self.device)
-        
-        #self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
-        self.optimizer  = optim.SGD(self.model.parameters(), lr=self.lr, momentum=0.9, weight_decay=0.0005)
-
+        self.model  = self.get_model()
+        self.model  = self.model.to(self.device)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+        self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer)
         self.iou = JaccardIndex(num_classes=self.classes, threshold=0.5)
-    
-    def get(self):
-        if self.network == 'fasterrcnn':
-            model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights='DEFAULT')
+        
+    def get_model(self):
+        if self.name == 'fasterrcnn':
+            model       = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights='DEFAULT')
             in_features = model.roi_heads.box_predictor.cls_score.in_features
             model.roi_heads.box_predictor = FastRCNNPredictor(in_features, self.classes)
             return model
 
-        if self.network == 'retinanet':
-            return torchvision.models.detection.retinanet_resnet50_fpn(weights_backbone='DEFAULT', num_classes=self.classes)
+        if self.name == 'retinanet':
+            return torchvision.models.detection.retinanet_resnet50_fpn(weights='DEFAULT')
 
-        if self.network == 'ssd':
-            return torchvision.models.detection.ssd300_vgg16(weights_backbone='DEFAULT', num_classes=self.classes)
+        if self.name == 'ssd':
+            return torchvision.models.detection.ssd300_vgg16(weights='DEFAULT')
+
+        if self.name == 'maskrcnn':
+            return torchvision.models.detection.maskrcnn_resnet50_fpn(weights='DEFAULT')
 
         return None
